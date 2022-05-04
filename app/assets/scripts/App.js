13debee8b351f6e4935d7da90e2e66e4e3c6f2e9
munkhuush <omunkhuush01@gmail.com>
2022-05-04 12:55:16 +0400

    sticky headerig duusgalaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,15 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/mobile-menu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
+
 if (module.hot) {
   module.hot.accept();
 }
 let mobileMenu = new MobileMenu();
+// -------- reveal scroll ------//
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 85);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+
+// ---- Sticky Header ----------//
+let stickyHeader = new StickyHeader();
