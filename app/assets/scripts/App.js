fef0b3166f4e8429814e17f6e605dfb6e54b78a0
munkhuush <omunkhuush01@gmail.com>
2022-05-04 15:57:30 +0400

    modalig duusgalaa.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import "../styles/styles.css";
 import MobileMenu from "./modules/mobile-menu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+//import Modal from "./modules/modal";
 
 if (module.hot) {
   module.hot.accept();
@@ -13,3 +14,23 @@ new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
 // ---- Sticky Header ----------//
 let stickyHeader = new StickyHeader();
+//---------Modal -------------//
+//new Modal();
+
+// webpack split modal --------//
+let modal;
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/*webpackChunkName: "modal"*/ "./modules/modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openModal(), 20);
+        })
+        .catch(() => console.log("file import error"));
+    } else {
+      modal.openModal();
+    }
+  });
+});
