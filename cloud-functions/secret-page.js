f6440f1fe4d53f3474001a8817453a03e49c892c
munkhuush <omunkhuush01@gmail.com>
2022-05-06 16:46:29 +0400

    cloud function update xiilee

--- cloud-functions/secret-page.js
+++ cloud-functions/secret-page.js
@@ -1,6 +1,22 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "hereglegchdiin xuudsand or",
-  });
+  const secretContent = `
+    <h3> cxereglegchiin tusgai xuudsand tawtai moril</h3>
+    <p> zowxon bvrtgelttei xereglegchid <strong>zoriulaw</strong></p>
+    `;
+  let body;
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+  if (body.password == "JavaScript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
